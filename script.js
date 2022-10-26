fc9a201b3fdfc982a773e34d603f45b222bf10aa
leopvr <leo.pvr13@gmail.com>
2022-10-25 21:59:38 -0300

    :sparkles: Add: array methods with examples

--- script.js
+++ script.js
@@ -232,12 +232,114 @@ const jsonPlaceholder = [
   ]
 console.log(jsonPlaceholder)
 
+//Forma 1
 // for (let i = 0; i < jsonPlaceholder.length; i++) {
 //     let nameArray = jsonPlaceholder[i];  
 //     console.log(`El usuario ${nameArray.id} se llama: ${nameArray.name}, su username: ${nameArray.username}, y vive en: ${nameArray.address.street} con latitud ${nameArray.address.geo.lat} y longitud ${nameArray.address.geo.lng}`); 
 // }
 
+//Forma 2
 for (let i = 0; i < jsonPlaceholder.length; i++) {
   console.log(`El usuario ${jsonPlaceholder[i].id} se llama: ${jsonPlaceholder[i].name}, su username: ${jsonPlaceholder[i].username}, y vive en: ${jsonPlaceholder[i].address.street} con latitud ${jsonPlaceholder[i].address.geo.lat} y longitud ${jsonPlaceholder[i].address.geo.lng}`); 
 }
 
+//------------------------------------------------------------------
+// [Métodos de Arrays]
+// - .pop
+// - .shift
+// - .unshift
+// - .find
+// - .some
+// - .push
+// - .sort
+// - .includes
+// - .join
+// - .indexOf
+// - .slice
+// - .reverse
+// - .from
+// - .concat
+
+//.pop----------------------------------
+// var popped = jsonPlaceholder.pop();
+// console.log(jsonPlaceholder, popped.name);
+
+
+//.shift----------------------------------
+// var eliminado = jsonPlaceholder.shift();
+// console.log(jsonPlaceholder, eliminado.name);
+
+
+//.unshift----------------------------------
+// var addUsers = jsonPlaceholder.unshift("Leo Peñalver", "Fabi Tureo");
+// console.log(jsonPlaceholder, addUsers.name);
+
+
+//.find----------------------------------
+// var found = jsonPlaceholder.find(e => e.name === "Leanne Graham");
+// console.log(jsonPlaceholder, found.name);
+
+
+//.some----------------------------------
+// const numbersArray = [1, 2, 3, 4, 5];
+// const even = (e) => e % 2 === 0;
+// console.log(numbersArray.some(even)); //Output: true
+
+
+//.push----------------------------------
+// var pushUsers = jsonPlaceholder.push("Leo Peñalver", "Fabi Tureo");
+// console.log(jsonPlaceholder, pushUsers);
+
+
+//.sort----------------------------------
+// const months = ['March', 'Jan', 'Feb', 'Dec'];
+// months.sort();
+// console.log(months); // Output: ['Dec', 'Feb', 'Jan', 'March']
+
+
+// const numbers = [1, 30, 4, 21, 100000];
+// numbers.sort();
+// console.log(numbers); // Output: [1, 100000, 21, 30, 4]
+
+
+//.includes----------------------------------
+// const numbers = [1, 2, 3];
+// console.log(numbers.includes(2)); // Output: true
+
+
+//.join----------------------------------
+// const elements = ['Fire', 'Air', 'Water'];
+// console.log(elements.join()); // Output: "Fire,Air,Water"
+// console.log(elements.join('')); // Output: "FireAirWater"
+// console.log(elements.join('-')); // Output: "Fire-Air-Water"
+
+
+//.indexOf----------------------------------
+// var array = [2, 9, 9];
+// array.indexOf(2);     // Output: 0
+// array.indexOf(7);     // Output: -1
+// array.indexOf(9, 2);  // Output: 2
+// array.indexOf(2, -1); // Output: -1
+// array.indexOf(2, -3); // Output: 0
+
+
+//.slice----------------------------------
+// var nombres = ['Rita', 'Pedro', 'Miguel', 'Ana', 'Vanesa'];
+// var masculinos = nombres.slice(1, 3); 
+// console.log(masculinos) // Output: masculinos contiene ['Pedro','Miguel']
+
+//.reverse----------------------------------
+// const months = ['March', 'Jan', 'Feb', 'Dec'];
+// months.reverse();
+// console.log(months); // Output: ['Dec', 'Feb', 'Jan', 'March']
+
+
+//.from----------------------------------
+// console.log(Array.from('foo')); // Output: Array ["f", "o", "o"]
+// console.log(Array.from([1, 2, 3], x => x + x)); // Output: Array [2, 4, 6]
+
+//.concat----------------------------------
+// const array1 = ['a', 'b', 'c'];
+// const array2 = ['d', 'e', 'f'];
+// const array3 = array1.concat(array2);
+// console.log(array3); // Output: Array ["a", "b", "c", "d", "e", "f"]
